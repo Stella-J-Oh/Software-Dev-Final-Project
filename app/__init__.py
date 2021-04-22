bd37600f3d5029ead07c23fc2b8172d521616a2b
Stella Oh <soh11@stuy.edu>
2021-04-21 21:33:32 -0400

    update repo structure

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+'''
+Team xoxo :: Stella Oh, Constance Chen, Winnie Huang, Helena Williams
+SoftDev
+P3: ArRESTed Development, JuSt in Time
+2021-04-21
+'''
+
+if __name__ == "__main__": # true is this file is NOT imported
+    app.debug = True #enable auto-reload upon code change
+    app.run()
